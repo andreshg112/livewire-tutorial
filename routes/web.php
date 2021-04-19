1bc1d084f9c4a36892a90c407eb6128111002174
Andrés Herrera García <andreshg112@gmail.com>
2021-04-18 19:24:15 -0500

    Register Component/Page

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,11 @@
 
 use App\Http\Livewire\Home;
 use App\Http\Livewire\Login;
+use App\Http\Livewire\Register;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', Home::class);
 
 Route::get('/login', Login::class);
+
+Route::get('/register', Register::class);
